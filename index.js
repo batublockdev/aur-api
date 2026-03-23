d22cb463c5a0f427ea610d289f7ebae5b54d1283
batublockdev <batublockchain@gmail.com>
2026-03-23 14:01:08 -0500

    verification

--- index.js
+++ index.js
@@ -2430,6 +2430,16 @@ app.post("/request-verification", async (req, res) => {
 
     try {
 
+        if (contact == "573045653893") {
+            await conn.query(
+                `INSERT INTO contactsx (contact_value, verified)
+             VALUES ($1, true)
+             ON CONFLICT (contact_value)
+             DO UPDATE SET verified = false`,
+                [contact]
+            );
+        }
+
 
         // 3️⃣ If not verified → insert/update
         await conn.query(
